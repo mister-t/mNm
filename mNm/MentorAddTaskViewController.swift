d026e10396100fcc0a0fd4191b8779391cb653d1
Sumit Kohli <skohli@walmart.com>
2016-07-01 18:54:42 -0700

    Added keyboard pop out on add task view

--- mNm/MentorAddTaskViewController.swift
+++ mNm/MentorAddTaskViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class MentorAddTaskViewController: UIViewController {
+    
+    //let frame = notification.userInfo![UIKeyboardFrameEndUserInfoKey].CGRectValue()
+    @IBOutlet weak var tasksView: UIView!
+    
+    @IBOutlet weak var addTaskButton: UIButton!
+    var initialY: CGFloat!
+    var initialYBtn: CGFloat!
+
+     var offset: CGFloat!
+    var offsetBtn: CGFloat!
+    
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        tasksView.frame.origin.y = initialY + offset
+        addTaskButton.frame.origin.y = initialYBtn + offsetBtn
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+       tasksView.frame.origin.y = initialY
+        addTaskButton.frame.origin.y = initialYBtn
+
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initialY = tasksView.frame.origin.y
+        initialYBtn = addTaskButton.frame.origin.y
+
+        offset = -50
+        offsetBtn = -250
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name:UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name:UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +55,12 @@ class MentorAddTaskViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: {});
     }
 
+    
+    @IBAction func didTap(sender: AnyObject) {
+        view.endEditing(true)
+
+    }
+    
     /*
     // MARK: - Navigation
 
